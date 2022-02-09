c6913d34af80d24580b22729c03f523d4a11ce8d
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2022-02-09 13:37:19 -0300

    @therealkaslow [added readme details]

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
-# League-Chat
+<div align="center" id="top"> 
+  <img src="./.github/app.gif" alt="League Chat" />
+
+  &#xa0;
+
+   <a href="https://league-chat.vercel.app">App</a> 
+</div>
+
+<h1 align="center">League Chat</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/{{therealkaslow}}/sql-dictionary?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/{{therealkaslow}}/sql-dictionary?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/{{therealkaslow}}/sql-dictionary?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/{{therealkaslow}}/sql-dictionary?color=56BEB8">
+
+  <!-- <img alt="Github issues" src="https://img.shields.io/github/issues/{{YOUR_GITHUB_USERNAME}}/sql-dictionary?color=56BEB8" /> -->
+
+  <!-- <img alt="Github forks" src="https://img.shields.io/github/forks/{{YOUR_GITHUB_USERNAME}}/sql-dictionary?color=56BEB8" /> -->
+
+  <img alt="Github stars" src="https://img.shields.io/github/stars/{{therealkaslow}}/sql-dictionary?color=56BEB8" /> 
+</p>
+
+<!-- Status -->
+
+ <h4 align="center"> 
+	🚧  SQL Dictionary 🚀 Em Construção...  🚧
+</h4> 
+
+<hr> 
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#memo-license">Licensa</a> &#xa0; | &#xa0;
+  <a href="https://github.com/{{therealkaslow}}" target="_blank">therealkaslow</a>
+  <a href="https://github.com/{{gc-barros}}" target="_blank">therealkaslow</a>
+</p>
+
+<br>
+
+## :dart: About ##
+
+Um chat baseado no jogo league of legends com consumo de API do github desenvolvido em Reactjs durante a semana da Imersão React da Alura
+
+## :rocket: Technologies ##
+
+The following tools were used in this project:
+
+- [Node.js](https://nodejs.org/en/)
+- [React](https://pt-br.reactjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+
+## :white_check_mark: Requirements ##
+
+Antes de começar :checkered_flag:, você precisa ter o [Git](https://git-scm.com) e o [Node](https://nodejs.org/en/) instalados.
+
+## :checkered_flag: Starting ##
+
+```bash
+# Clone this project
+$ git clone https://github.com/{{therealkaslow}}/League-Chat
+
+# Acesso
+$ cd League-Chat
+
+# Instalar dependencias
+$ yarn
+
+# Rodar o projeto
+$ yarn dev
+
+# o servidor vai iniciar no <http://localhost:3000>
+```
+
+## :memo: License ##
+
+This project is under license from MIT. For more details, see the [LICENSE](LICENSE.md) file.
+
+
+Made with :heart: by <a href="https://github.com/{{therealkaslow}}" target="_blank">{{therealkaslow}}</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
--- pages/chat.js
+++ pages/chat.js
@@ -1,7 +1,290 @@
-import globalcss from '../pages/_app'
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import React from 'react';
+import appConfig from '../config.json';
+import { useRouter } from 'next/router';
+import { createClient } from '@supabase/supabase-js'
+import { ButtonSendSticker } from '../src/components/ButtonSendSticker';
 
-export default function ChatPage(){
-    return(
-        <div>Página do Xat</div>
-    )
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzMwNDg4NSwiZXhwIjoxOTU4ODgwODg1fQ.SfK-Fqlj3GbOh4HpeCEocI4VykxuRG8_vr1w3rtIceg'
+const SUPABASE_URL = 'https://wtvulhstzhhbasfeyeor.supabase.co'
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+
+function escutaMensagensEmTempoReal(adicionaMensagem) {
+  return supabaseClient
+    .from('mensagens')
+    .on('INSERT', (respostaLive) => {
+      adicionaMensagem(respostaLive.new);
+    })
+    .subscribe();
+}
+
+export default function ChatPage() {
+  const roteamento = useRouter();
+  const usuarioLogado = roteamento.query.username;
+  const [mensagem, setMensagem] = React.useState('');
+  const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
+
+  React.useEffect(() => {
+    supabaseClient
+      .from('mensagens')
+      .select('*')
+      .order('id', { ascending: false })
+      .then(({ data }) => {
+        // console.log('Dados da consulta:', data);
+        setListaDeMensagens(data);
+      });
+
+    const subscription = escutaMensagensEmTempoReal((novaMensagem) => {
+      console.log('Nova mensagem:', novaMensagem);
+      console.log('listaDeMensagens:', listaDeMensagens);
+      // Quero reusar um valor de referencia (objeto/array) 
+      // Passar uma função pro setState
+
+      // setListaDeMensagens([
+      //     novaMensagem,
+      //     ...listaDeMensagens
+      // ])
+      setListaDeMensagens((valorAtualDaLista) => {
+        console.log('valorAtualDaLista:', valorAtualDaLista);
+        return [
+          novaMensagem,
+          ...valorAtualDaLista,
+        ]
+      });
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    }
+  }, []);
+
+  function handleNovaMensagem(novaMensagem) {
+    const mensagem = {
+      // id: listaDeMensagens.length + 1,
+      de: usuarioLogado,
+      texto: novaMensagem,
+    };
+
+    supabaseClient
+      .from('mensagens')
+      .insert([
+        // Tem que ser um objeto com os MESMOS CAMPOS que você escreveu no supabase
+        mensagem
+      ])
+      .then(({ data }) => {
+        console.log('Criando mensagem: ', data);
+      });
+
+    setMensagem('');
+  }
+
+  return (
+    <Box
+    styleSheet={{
+        display: 'flex', alignItems: 'center', justifyContent: 'center',
+        backgroundColor: appConfig.theme.colors.primary[100],
+        backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/07/star-wars-the-lars-homestead-at-tatooine.jpg)',
+        backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+    }}
+    >
+      <Box
+        styleSheet={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+          borderRadius: '5px',
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: '100%',
+          maxWidth: '95%',
+          maxHeight: '95vh',
+          padding: '32px',
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{
+            position: 'relative',
+            display: 'flex',
+            flex: 1,
+            height: '80%',
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: 'column',
+            borderRadius: '5px',
+            padding: '16px',
+          }}
+        >
+          <MessageList mensagens={listaDeMensagens} />
+          {/* {listaDeMensagens.map((mensagemAtual) => {
+                        return (
+                            <li key={mensagemAtual.id}>
+                                {mensagemAtual.de}: {mensagemAtual.texto}
+                            </li>
+                        )
+                    })} */}
+          <Box
+            as="form"
+            styleSheet={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <TextField
+              value={mensagem}
+              onChange={(event) => {
+                const valor = event.target.value;
+                setMensagem(valor);
+              }}
+              onKeyPress={(event) => {
+                if (event.key === 'Enter') {
+                  event.preventDefault();
+                  handleNovaMensagem(mensagem);
+                }
+              }}
+              placeholder="Insira sua mensagem aqui..."
+              type="textarea"
+              styleSheet={{
+                width: '100%',
+                border: '0',
+                resize: 'none',
+                borderRadius: '5px',
+                padding: '6px 8px',
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: '12px',
+                color: appConfig.theme.colors.neutrals[200],
+              }}
+            />
+            {/* CallBack */}
+            <ButtonSendSticker
+              onStickerClick={(sticker) => {
+                // console.log('[USANDO O COMPONENTE] Salva esse sticker no banco', sticker);
+                handleNovaMensagem(':sticker: ' + sticker);
+              }}
+            />
+            <Button
+              styleSheet={{
+                borderRadius: '50%',
+                padding: '0 3px 0 0',
+                minWidth: '50px',
+                minHeight: '50px',
+                fontSize: '20px',
+                marginBottom: '8px',
+                marginLeft: '4px',
+                lineHeight: '0',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: appConfig.theme.colors.neutrals[300],
+                hover: {
+                  filter: 'grayscale(0)',
+                }
+              }}
+              label='✔️'
+              onClick={() => {
+                handleNovaMensagem(mensagem);
+                
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+function Header() {
+  return (
+    <>
+      <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+        <Text variant='heading5'>
+          Chat
+        </Text>
+        <Button
+          variant='tertiary'
+          colorVariant='neutral'
+          label='Logout'
+          href="/"
+        />
+      </Box>
+    </>
+  )
+}
+
+function MessageList(props) {
+  // console.log(props);
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: 'scroll',
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["000"],
+        marginBottom: '16px',
+      }}
+    >
+      {props.mensagens.map((mensagem) => {
+        return (
+          <Text
+            key={mensagem.id}
+            tag="li"
+            styleSheet={{
+              borderRadius: '5px',
+              padding: '6px',
+              marginBottom: '12px',
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+              }
+            }}
+          >
+            <Box
+              styleSheet={{
+                marginBottom: '8px',
+              }}
+            >
+              <Image
+                styleSheet={{
+                  width: '20px',
+                  height: '20px',
+                  borderRadius: '50%',
+                  display: 'inline-block',
+                  marginRight: '8px',
+                }}
+                src={`https://github.com/${mensagem.de}.png`}
+              />
+              <Text tag="strong">
+                {mensagem.de}
+              </Text>
+              <Text
+                styleSheet={{
+                  fontSize: '10px',
+                  marginLeft: '8px',
+                  color: appConfig.theme.colors.neutrals[300],
+                }}
+                tag="span"
+              >
+                {(new Date().toLocaleDateString())}
+              </Text>
+            </Box>
+            {/* [Declarativo] */}
+            {/* Condicional: {mensagem.texto.startsWith(':sticker:').toString()} */}
+            {mensagem.texto.startsWith(':sticker:')
+              ? (
+                <Image src={mensagem.texto.replace(':sticker:', '')} />
+              )
+              : (
+                mensagem.texto
+              )}
+            {/* if mensagem de texto possui stickers:
+                           mostra a imagem
+                        else 
+                           mensagem.texto */}
+            {/* {mensagem.texto} */}
+          </Text>
+        );
+      })}
+    </Box>
+  )
 }
